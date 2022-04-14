848fd895d2d4fba2ef59deb7204ecf73795394d5
Javier Sánchez <javier.sanchezortiz@xploregroup.es>
2022-04-14 18:09:11 +0200

    chore: Modify Angular to avoid error in scss size budget

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  space: '8cn2oov2dxi6',
+  accessToken: '2uBuES7-PIsKXW774Vq5FKQWJCOZd5kXLGsmnea1iSU',
+  contentTypeIds: {
+    track: 'track',
+  },
 };
